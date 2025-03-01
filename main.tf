3b81ab07d8d058901453eff93c582f9a7b906e3e
alibaba0010 <zakariyahali100@gmail.com>
2025-03-01 10:49:28 +0100

    feat: added local-exec to run ansible in main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "app_server" {
   }
 provisioner "local-exec" {
   command = <<EOT
-    echo "[servers]" > ansible/inventory
+    echo "[app_server]" > ansible/inventory
      echo "app_server ansible_host=${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${var.ssh_key_path} ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> ansible/inventory
   EOT
 }
@@ -53,4 +53,26 @@ resource "aws_security_group" "app_sg" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ansible/inventory ansible/site.yml"
+  }
+}
+resource "null_resource" "wait_for_ssh" {
+  depends_on = [aws_instance.app_server]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "Waiting for SSH to be ready..."
+      sleep 30
+    EOT
+  }
+}
+
+resource "null_resource" "configure_server" {
+  depends_on = [null_resource.wait_for_ssh]
+
+provisioner "local-exec" {
+    command = "ansible-playbook -i ansible/inventory ansible/site.yml"
+  }
+  
 }
